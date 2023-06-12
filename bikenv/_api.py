295974d663e4b2fd1120f59bc160396d78e5beef
Fabián Abarca <ensinergia@gmail.com>
2023-06-12 16:23:14 -0600

    Exponer las funciones en el namespace bikenv.*

---
+++ bikenv/_api.py
@@ -0,0 +1,5 @@
+"""Expose most common parts of public API directly in `bikenv.` namespace."""
+
+from .module import get_region
+from .module import altitude_index
+from .module import distance_index

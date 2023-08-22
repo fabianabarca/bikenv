b20045631aaed9d41780730929e496af705ce823
Fabián Abarca <ensinergia@gmail.com>
2023-08-22 18:55:07 -0300

    Create directory for tests (in the future) with an example file

---
+++ tests/example.py
@@ -0,0 +1,39 @@
+import bikenv
+
+print("This is an example of how to use the package.")
+
+# Get the graph of the region of interest
+G = bikenv.get_region("Cinco Esquinas de Tibás, San José, Costa Rica")
+
+print("This is the graph of the region of interest.")
+
+# Get the altitude index of the region of interest
+normalized_elevation = bikenv.altitude_index(G, google_key="AIzaSyB2WP2FoMe4DRz20FsGGi51opPlfjcm4KM")
+
+print("This is the altitude index of the region of interest.")
+
+# Get the stats of the altitude index
+stats = bikenv.normalized_elevation_stats(normalized_elevation)
+
+print("Stats of the altitude index of the region of interest.")
+print(stats)
+
+# Plot the histogram of the altitude index
+bikenv.normalized_elevation_hist(normalized_elevation)
+
+
+# With OOP:
+
+# Create a region object
+tibas = bikenv.Region("Cinco Esquinas de Tibás, San José, Costa Rica", google_key="")
+print("This is the graph of the region of interest.")
+
+# Get the indexes of the region of interest
+tibas.altitude_index
+tibas.distance_index
+
+# Get the stats of the altitude index (possibly must change in the future)
+tibas.normalized_elevation_stats()
+
+# Plot the histogram of the altitude index
+tibas.normalized_elevation_hist()

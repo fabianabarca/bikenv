f69aee40e914413a816f1c915630bdeed8f12e39
DerianMonge <derian.monge@ucr.ac.cr>
2023-09-05 13:29:48 -0600

    Class Region added

--- bikenv/_api.py
+++ bikenv/_api.py
@@ -1,7 +1,3 @@
 """Expose most common parts of public API directly in `bikenv.` namespace."""
 
-from .module import get_region
-from .module import altitude_index
-from .module import normalized_elevation_stats
-from .module import normalized_elevation_hist
-from .module import distance_index
+from .module import Region
